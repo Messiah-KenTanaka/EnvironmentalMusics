c556a987b21a3153a5859b2ec52b7d519e63d499
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2022-02-08 13:20:33 +0900

    Initial commit

---
+++ app/src/main/java/com/beit_and_pear/android/environmentalmusics/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.beit_and_pear.android.environmentalmusics
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
