8ec781dd783fd0401a1e6bcd6f7f32595afea4f0
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2022-12-30 17:02:58 +0900

    タイトルをnull許容に変更し投稿画面から項目をコメントアウト

--- app/Http/Requests/ArticleRequest.php
+++ app/Http/Requests/ArticleRequest.php
@@ -24,7 +24,7 @@ class ArticleRequest extends FormRequest
     public function rules()
     {
         return [
-            'title' => 'required|max:50',
+            // 'title' => 'required|max:50',
             'body' => 'required|max:500',
             'tags' => 'json|regex:/^(?!.*\s).+$/u|regex:/^(?!.*\/).*$/u',
         ];
@@ -33,7 +33,7 @@ class ArticleRequest extends FormRequest
     public function attributes()
     {
         return [
-            'title' => 'タイトル',
+            // 'title' => 'タイトル',
             'body' => '本文',
             'tags' => 'タグ',
         ];
