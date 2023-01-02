dc4105c54104af415786f5d2119beb51b6bc5721
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2023-01-02 17:02:46 +0900

    記事を投稿へ文言を変更

--- resources/views/articles/card.blade.php
+++ resources/views/articles/card.blade.php
@@ -26,11 +26,11 @@
           <div class="dropdown-menu dropdown-menu-right">
             {{--  更新はできないようにコメントアウト
             <a class="dropdown-item" href="{{ route("articles.edit", ['article' => $article]) }}">
-              <i class="fas fa-pen mr-1"></i>記事を更新する
+              <i class="fas fa-pen mr-1"></i>投稿を更新する
             </a>
             <div class="dropdown-divider"></div>  --}}
             <a class="dropdown-item text-danger" data-toggle="modal" data-target="#modal-delete-{{ $article->id }}">
-              <i class="fas fa-trash-alt mr-1"></i>記事を削除
+              <i class="fas fa-trash-alt mr-1"></i>投稿を削除
             </a>
           </div>
         </div>
