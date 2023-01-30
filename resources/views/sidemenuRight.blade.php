0b7a25771562ca410703aa5e8334ee9c85dd8ec2
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2023-01-30 21:48:31 +0900

    人気のタグのマージンを変更

--- resources/views/sidemenuRight.blade.php
+++ resources/views/sidemenuRight.blade.php
@@ -1,5 +1,5 @@
 <div class="col-3 mt-5 d-none d-md-block">
-    <div class="card form-group sidemenu-fixed py-3 px-4">
+    <div class="card form-group sidemenu-fixed p-3">
         <span class="dropdown-item font-weight-bold py-2"><i class="fa-solid fa-tags mr-2"></i>人気のタグ</span>
         <div class="dropdown-divider"></div>
         @foreach($tags as $tag)
