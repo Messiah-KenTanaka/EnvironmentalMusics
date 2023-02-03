2ca1b8411e0c667a95cb7ea99c503c5a8eb576b5
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2023-02-03 21:43:50 +0900

    ナビボトムの人気のタグをドロップアップに変更

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,27 +1,6 @@
 <nav class="nav navbar nav-fixed navbar-expand navbar-light dusty-grass-gradient">
   <a class="navbar-brand main-font-family" href="/">BASSER</a>
-  <ul class="navbar-nav ml-auto">
-    {{-- タグ検索 --}}
-    <li class="nav-item">
-      <!-- Dropdown -->
-      <li class="nav-item dropdown">
-        <a class="nav-link" id="navbarDropdownTagLink" data-toggle="dropdown"
-          aria-haspopup="true" aria-expanded="false">
-          <i class="fas fa-search"></i>
-        </a>
-        <div class="dropdown-menu dropdown-menu-right dropdown-primary" aria-labelledby="navbarDropdownTagLink">
-          <span class="dropdown-item font-weight-bold py-2"><i class="fa-solid fa-tags mr-2"></i>人気のタグ</span>
-          <div class="dropdown-divider"></div>
-          @foreach($tags as $tag)
-            <button class="dropdown-item" type="button"
-                    onclick="location.href='{{ route('tags.show', ['name' => $tag->name]) }}'">
-              <span class="ml-1">{{ Functions::getNameTenEllipsis($tag->name) }}</span>
-            </button>
-          @endforeach
-        </div>
-      </li>
-      <!-- Dropdown -->
-    </li>   
+  <ul class="navbar-nav ml-auto"> 
     @guest
     <li class="nav-item">
       <a class="nav-link" href="{{ route('register') }}"><i class="fa-solid fa-user-plus"></i></a>
