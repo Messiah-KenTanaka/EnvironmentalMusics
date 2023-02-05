070ca1a43f3dc17c8157601fb515898bc9b77cf5
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2023-02-05 21:43:53 +0900

    ボタンクリックでローディングを追加

--- resources/js/loading.js
+++ resources/js/loading.js
@@ -1,3 +1,4 @@
+// 画面遷移時ローディング
 $(function () {
     //ローディング表示
     show_loading();
@@ -7,13 +8,22 @@ $(function () {
         hide_loading();
     }, 1000);
 })
-
 //ローディング表示
 function show_loading() {
     $('#loading').removeClass('d-none');
 }
-
 //ローディング非表示
 function hide_loading() {
     $('#loading').addClass('d-none');
 }
+
+// ボタンローディング 投稿・プロフ更新
+$(function () {
+    $(".loading-btn").on("click", function(){
+        console.log('hoge');
+        $("#overlay").fadeIn(500); //二度押しを防ぐloading表示
+        setTimeout(function(){
+            $("#overlay").fadeOut(500);
+        },10000);
+    });
+})
