433b88053c0a9c3166eff1558ed70f32824c7a55
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2023-02-12 18:40:27 +0900

    タグ一覧を投稿順、ページネーション機能を追加

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -13,10 +13,17 @@ class TagController extends Controller
         $tag = Tag::where('name', $name)->first()
             ->load(['articles.user', 'articles.likes', 'articles.tags']);
 
+        $articles = $tag->articles
+            ->sortByDesc('created_at')
+            ->paginate(config('paginate.paginate'));
+
+        // dd($tag);
+
         $tags = Tag::getPopularTag();
 
         return view('tags.show',[
             'tag' => $tag,
+            'articles' =>$articles,
             'tags' => $tags,
         ]);
     }
