24c904e9912e4af2a4c2dc31ee7593ed30703bd6
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2023-02-28 21:58:51 +0900

    投稿時、ボタンクリックでローディングが走る機能をリバート(クリックでなんでもローディングが走ってしまうため)

--- resources/js/loading.js
+++ resources/js/loading.js
@@ -1,4 +1,3 @@
-// 画面遷移時ローディング
 $(function () {
     //ローディング表示
     show_loading();
@@ -8,21 +7,13 @@ $(function () {
         hide_loading();
     }, 1000);
 })
+
 //ローディング表示
 function show_loading() {
     $('#loading').removeClass('d-none');
 }
+
 //ローディング非表示
 function hide_loading() {
     $('#loading').addClass('d-none');
 }
-
-// ボタンローディング 投稿・プロフ更新
-$(function () {
-    $(".loading-btn").on("click", function(){
-        $("#overlay").fadeIn(500); //二度押しを防ぐloading表示
-        setTimeout(function(){
-            $("#overlay").fadeOut(500);
-        },10000);
-    });
-})
