87f1d18bf6eeef99c03a7bb0af204c06eb8af1c3
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2023-04-02 11:51:06 +0900

    検索結果一覧の上部に検索キーワードと件数を表示。

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -8,6 +8,17 @@
         <div class="row">
         @include('sidemenu')
         <div class="col">
+            <h2 class="text-center mt-5 my-3 main-ja-font-family search-heading" data-en="Search"><span>検索</span></h2>
+            <div class="card mt-3">
+                <div class="card-body">
+                    <h2 class="h4 card-title m-0">
+                        {{ $keyword }}
+                    </h2>
+                    <div class="card-text text-right">
+                        {{ $results->count() }}件
+                    </div>
+                </div>
+            </div>
             @foreach($results as $key => $article)
                 @include('articles.card')
             @endforeach
