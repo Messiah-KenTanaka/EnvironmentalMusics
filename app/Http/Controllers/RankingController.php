c24b6de3c3022acc84a9f9ae6a0a30759eec76c0
KenTanaka <k.tanaka@apptime.co.jp>
2023-06-02 10:58:43 +0900

    各ページにてブロックしたユーザーを非表示にするよう修正

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -5,15 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Article;
 use App\Tag;
+use App\BlockList;
 
 class RankingController extends Controller
 {
     public function index()
     {
+        $userId = auth()->id(); // ログインユーザーのIDを取得
+
+        // ブロックリストからブロックしたユーザーのIDを取得
+        $blockUsers = BlockList::where('user_id', $userId)->pluck('blocked_user_id');
+
         // 全国ランキング
         $ranking = Article::with(['user', 'likes', 'tags'])
-            ->whereHas('user', function ($query) {
-                $query->where('publish_flag', 1);
+            ->whereHas('user', function ($query) use ($blockUsers) {
+                $query->where('publish_flag', 1)
+                    ->whereNotIn('user_id', $blockUsers); // ブロックしたユーザーを除外
             })
             ->where('publish_flag', 1)
             ->whereNotNull('image')
@@ -33,10 +40,16 @@ class RankingController extends Controller
 
     public function show(string $pref)
     {
+        $userId = auth()->id(); // ログインユーザーのIDを取得
+
+        // ブロックリストからブロックしたユーザーのIDを取得
+        $blockUsers = BlockList::where('user_id', $userId)->pluck('blocked_user_id');
+
         // 都道府県ランキング
         $ranking = Article::with(['user', 'likes', 'tags'])
-            ->whereHas('user', function ($query) {
-                $query->where('publish_flag', 1);
+            ->whereHas('user', function ($query) use ($blockUsers) {
+                $query->where('publish_flag', 1)
+                    ->whereNotIn('user_id', $blockUsers); // ブロックしたユーザーを除外
             })
             ->where('publish_flag', 1)
             ->where('pref', $pref)
