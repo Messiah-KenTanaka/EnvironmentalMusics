be91e2b379d49dadf3d7a2efbe03a01116f5abdb
KenTanaka <k.tanaka@apptime.co.jp>
2023-06-30 14:14:08 +0900

    metaタグを各種設定する

---
+++ config/meta.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'general' => [
+        'title' => 'BASSER',
+        'description' => 'BASSERはブラックバス釣り専用のSNSアプリです。釣果報告、コメントのやり取り、ランキング、釣り場マップ、天気予報など、ブラックバス釣りに必要な全ての情報を提供します。',
+        'keywords' => 'バス釣り, 釣果報告, SNS, ランキング, 釣り場マップ, 天気予報', '釣り', 'フィッシング',
+        'image' => env('APP_URL') . '/images/top_image01.jpg',
+        'url' => env('APP_URL'),
+    ],
+];
