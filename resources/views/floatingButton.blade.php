f0023741f713c6fb3241e61dc1480c99f601c3a8
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2023-07-11 21:01:31 +0900

    ヘッダー、フッターナビの色をblack#000000に変更

--- resources/views/floatingButton.blade.php
+++ resources/views/floatingButton.blade.php
@@ -1,6 +1,6 @@
 <!-- 画面右下に表示される投稿フローティングボタン -->
 <div class="floating-bottom d-block d-md-none">
-    <a class="btn bg-dark bg-gradient text-white rounded-circle p-3">
+    <a class="btn bg-black text-white rounded-circle p-3">
         <i class="fa-solid fa-rotate-left large-icon-2" onclick="window.history.back()" width="25" height="25"></i>
     </a>
 </div>
