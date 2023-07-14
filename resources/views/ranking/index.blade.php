4952b466f48dd71c1bdb1f6517b3796ead7258ad
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2023-07-14 20:08:24 +0900

    ランキングのレイアウトを変更

--- resources/views/ranking/index.blade.php
+++ resources/views/ranking/index.blade.php
@@ -11,22 +11,7 @@
         @include('ranking.pref')
         <h4 class="text-center my-3 main-ja-font-family"><span>全国 ランキング</span></h4>
         @foreach($ranking as $key => $article)
-          <div class="text-center my-4 main-ja-font-family h4">
-            @switch(++$key)
-              @case(1)
-                <img src="{{ asset('images/ranking_1.png')}}" class="rounded-circle mr-1" width="50" height="50">
-                  @break
-              @case(2)
-              <img src="{{ asset('images/ranking_2.png')}}" class="rounded-circle mr-1" width="50" height="50">
-                  @break
-              @case(3)
-              <img src="{{ asset('images/ranking_3.png')}}" class="rounded-circle mr-1" width="50" height="50">
-                  @break
-              @default
-                <span class="">{{ $key }}位</span>
-            @endswitch
-          </div>
-          @include('articles.card')
+          @include('ranking.card', ['rank' => ++$key])
         @endforeach
         @include('floatingButton')
       </div>
