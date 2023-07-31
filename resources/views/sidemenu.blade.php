dce498db0ee1ccb1cd729c265c95351f234c595e
KenTanaka <k.tanaka@apptime.co.jp>
2023-07-31 09:51:34 +0900

    サイドバーのレイアウトを変更

--- resources/views/sidemenu.blade.php
+++ resources/views/sidemenu.blade.php
@@ -1,10 +1,10 @@
 <div class="col-3 d-none d-xl-block">
-    <div class="form-group mt-3">
+    <div class="form-group nav-item mt-3">
         @include('search')
         <button class="dropdown-item py-2" type="button"
             onclick="location.href='/'">
             <i class="fas fa-home"></i>
-            <span class="ml-3 font-weight-bold">Home</span>
+            <span class=" font-weight-bold">Home</span>
         </button>
         <div class="dropdown-divider"></div>
 
@@ -12,7 +12,7 @@
             <button class="dropdown-item py-2" type="button"
                 onclick="location.href='{{ route('articles.create') }}'">
                 <i class="fa-solid fa-fish"></i>
-                <span class="ml-3 font-weight-bold">Post</span>
+                <span class=" font-weight-bold">Post</span>
             </button>
             <div class="dropdown-divider"></div>
         @endauth  --}}
@@ -21,50 +21,48 @@
         <button class="dropdown-item py-2" type="button" data-toggle="modal" data-target="#dmaintenanceModal"
             onclick="location.href='#'">
             <i class="fas fa-map-marker-alt"></i>
-            <span class="ml-3 pl-1 font-weight-bold">Map</span>
+            <span class=" pl-1 font-weight-bold">Map</span>
         </button>
         <div class="dropdown-divider"></div>  --}}
 
         <button class="dropdown-item py-2" type="button"
             onclick="location.href='{{ route('ranking.index') }}'">
             <i class="fas fa-crown"></i>
-            <span class="ml-3 font-weight-bold">Ranking</span>
+            <span class=" font-weight-bold">Ranking</span>
         </button>
         <div class="dropdown-divider"></div>
 
         <button class="dropdown-item py-2" type="button"
             onclick="location.href='{{ route('map.index') }}'">
             <i class="fas fa-map-marker-alt"></i>
-            <span class="ml-3 font-weight-bold">Map</span>
+            <span class=" font-weight-bold">Map</span>
         </button>
         <div class="dropdown-divider"></div>
 
         <button class="dropdown-item py-2" type="button"
             onclick="location.href='{{ route('weather.index') }}'">
             <i class="fa-solid fa-cloud-sun"></i>
-            <span class="ml-3 font-weight-bold">Weather</span>
+            <span class=" font-weight-bold">Weather</span>
         </button>
         <div class="dropdown-divider"></div>
 
         <button class="dropdown-item py-2" type="button"
             onclick="location.href='{{ route('search.index') }}'">
             <i class="fa-solid fa-magnifying-glass"></i>
-            <span class="ml-3 font-weight-bold">Search</span>
+            <span class=" font-weight-bold">Search</span>
         </button>
         <div class="dropdown-divider"></div>
-
-
 
         @auth
             <button class="dropdown-item py-2" type="button"
                 onclick="location.href='{{ route("users.show", ["name" => Auth::user()->name]) }}'">
                 <i class="fas fa-user-alt"></i>
-                <span class="ml-3 font-weight-bold">MyPage</span>
+                <span class=" font-weight-bold">MyPage</span>
             </button>
             <div class="dropdown-divider"></div>
             <button form="logout-button" class="dropdown-item py-2" type="submit">
                 <i class="fas fa-sign-in-alt"></i>
-                <span class="ml-3 font-weight-bold">Logout</span>
+                <span class=" font-weight-bold">Logout</span>
             </button>
             <div class="dropdown-divider"></div>
         @endauth
@@ -86,7 +84,7 @@
             <button class="dropdown-item py-2" type="button"
                 onclick="location.href='{{ route('register') }}'">
                 <i class="fa-solid fa-user-plus"></i>
-                <span class="ml-3 font-weight-bold">UserRegister</span>
+                <span class=" font-weight-bold">UserRegister</span>
             </button>
             <div class="dropdown-divider"></div>
         @endguest
@@ -95,14 +93,14 @@
             <button class="dropdown-item py-2" type="button"
                 onclick="location.href='{{ route('login') }}'">
                 <i class="fas fa-sign-in-alt"></i>
-                <span class="ml-3 font-weight-bold">Login</span>
+                <span class=" font-weight-bold">Login</span>
             </button>
             <div class="dropdown-divider"></div>
         @endguest
 
-        <button class="col-12 mx-auto btn bg-primary text-white rounded-pill" type="button"
+        <button class="col-12 mx-auto btn bg-dark text-white rounded-pill" type="button"
             onclick="location.href='{{ route('articles.create') }}'">
-            <img src="{{ asset('images/fish_icon.svg')}}" class="rounded-circle mr-1" width="25" height="25">
+            <i class="fa-solid fa-plus"></i>
         </button>
 
         <video class="mt-5" width="100%" controls autoplay loop muted>
@@ -111,12 +109,18 @@
 
         <span class="dropdown-item font-weight-bold mt-5 py-2"><i class="fas fa-bolt mr-2"></i>最近のトレンド</span>
         <div class="dropdown-divider"></div>
-        @foreach($tags as $tag)
-            <button class="dropdown-item py-1" type="button"
-                onclick="location.href='{{ route('tags.show', ['name' => $tag->name]) }}'">
-                {{ Functions::getNameTenEllipsis($tag->name) }}
-            </button>
-        @endforeach
+            @foreach($tags as $i => $tag)
+                <button class="dropdown-item d-flex align-items-start" type="button"
+                        onclick="location.href='{{ route('tags.show', ['name' => $tag->name]) }}'">
+                    <div>
+                        {{ ++$i }}.
+                    </div>
+                    <div>
+                        <span class="ml-1 font-weight-bold">{{ '#' . Functions::getNameTenEllipsis($tag->name) }}</span><br>
+                        <span class="text-muted">{{ $tag->count }}件</span>
+                    </div>
+                </button>
+            @endforeach
         <div class="dropdown-divider"></div>
 
     </div>
