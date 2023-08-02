1440bb122fea4bc587a83cf25ea877732777ba57
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2023-08-02 21:06:11 +0900

    タブの見た目を変更

--- resources/views/users/tabs.blade.php
+++ resources/views/users/tabs.blade.php
@@ -1,18 +1,18 @@
 <ul class="nav nav-tabs nav-justified mt-3">
     <li class="nav-item">
-        <a class="nav-link text-muted {{ $hasArticles ? 'active' : '' }}"
+        <a class="nav-link text-muted small {{ $hasArticles ? 'active' : '' }}"
             href="{{ route('users.show', ['name' => $user->name]) }}">
         投稿
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link text-muted {{ $hasLikes ? 'active' : '' }}"
+        <a class="nav-link text-muted small {{ $hasLikes ? 'active' : '' }}"
             href="{{ route('users.likes', ['name' => $user->name]) }}">
         いいね
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link text-muted {{ $hasConquest ? 'active' : '' }}"
+        <a class="nav-link text-muted small {{ $hasConquest ? 'active' : '' }}"
             href="{{ route('users.conquest', ['name' => $user->name]) }}">
         全国制覇
         </a>
