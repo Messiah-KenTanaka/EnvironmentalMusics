d4e9116a7340bd97a43dfff53bed16055c6d8190
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2023-10-07 19:27:11 +0900

    TOP画面の画像をslickで表示するように修正

---
+++ resources/js/components/TopImageSlider.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <vue-slick-carousel
+            :infinite="true"
+            :slides-to-show="slidesToShow"
+            :slides-to-scroll="1"
+            :dots="true"
+            :centerMode="false"
+            :prevArrow="prevArrow"
+            :nextArrow="nextArrow"
+            class="slider-container"
+        >
+            <div v-for="(image, index) in images" :key="index" class="image-container mt-3">
+                <img :src="image" alt="画像" class="img-fluid" />
+                <div class="text-overlay">
+                    <h3 class="main-font-family">BASSER</h3>
+                    <span>最高の釣りがここにある</span>
+                </div>
+            </div>
+        </vue-slick-carousel>
+    </div>
+</template>
+
+
+<script>
+import VueSlickCarousel from "vue-slick-carousel";
+import "vue-slick-carousel/dist/vue-slick-carousel.css";
+import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
+
+export default {
+    components: {
+        VueSlickCarousel,
+    },
+    data() {
+        return {
+            slidesToShow: 1, // 初期値
+            images: [
+                "/images/background/top_background_01.png",
+                "/images/background/top_background_02.png",
+                "/images/background/top_background_03.png",
+                "/images/background/top_background_04.png",
+            ],
+        }
+    },
+};
+</script>
+
+<style scoped>
+    img {
+        border-radius: 0.25rem;
+    }
+</style>
