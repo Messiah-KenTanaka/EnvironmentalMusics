96cb01aa93b61cffaacd379f0e58d3f8c0e19dc6
Messiah-KenTanaka <Tanaka.ken.3284@gmail.com>
2023-10-07 18:08:36 +0900

    トップの画像を変更

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -3,40 +3,40 @@
 @section('title', config('app.name') . ' | バス釣り専用SNS')
 
 @section('content')
-  @include('nav')
-  <div class="container">
-    <div class="row">
-      @include('sidemenu')
-      <div class="col col-xl-9">
-        @include('success_text')
-        @include('error_text')
-        <div class="form-group">
-          <div class="image-container mt-3 d-none d-xl-block">
-            <img src="{{ asset('images/top_image01.jpg')}}" class="img-fluid">
-            <div class="text-overlay">
-              <h3 class="main-font-family">BASSER</h3>
-              <span>最高の釣りがここにある</span>
+    @include('nav')
+    <div class="container">
+        <div class="row">
+            @include('sidemenu')
+            <div class="col col-xl-9">
+                @include('success_text')
+                @include('error_text')
+                <div class="form-group">
+                    <div class="image-container mt-3 d-none d-xl-block">
+                        <img src="{{ asset('images/background/top_background_01.png') }}" class="img-fluid">
+                        <div class="text-overlay">
+                            <h3 class="main-font-family">BASSER</h3>
+                            <span>最高の釣りがここにある</span>
+                        </div>
+                    </div>
+                    <a href="{{ route('articles.create') }}">
+                        <label></label>
+                        <div class="form-control" style="min-height: 20px;">今日はどうだった...？</div>
+                    </a>
+                </div>
+                @if ($articles->currentPage() == 1)
+                    @foreach ($retweetArticles as $article)
+                        @include('articles.card')
+                    @endforeach
+                @endif
+                @foreach ($articles as $article)
+                    @include('articles.card')
+                @endforeach
+                @if ($articles->hasMorePages())
+                    <p class="text-center mt-3 mb-5"><a href="{{ $articles->nextPageUrl() }}">もっと読み込む</a></p>
+                @endif
+                {{-- @include('floatingButton') --}}
             </div>
-          </div>
-          <a href="{{ route('articles.create') }}">
-            <label></label>
-            <div class="form-control" style="min-height: 20px;">今日はどうだった...？</div>
-          </a>
         </div>
-        @if ($articles->currentPage() == 1)
-          @foreach($retweetArticles as $article)
-            @include('articles.card')
-          @endforeach
-        @endif
-        @foreach($articles as $article)
-          @include('articles.card')
-        @endforeach
-        @if ($articles->hasMorePages())
-          <p class="text-center mt-3 mb-5"><a href="{{ $articles->nextPageUrl() }}">もっと読み込む</a></p>
-        @endif
-        {{-- @include('floatingButton') --}}
-      </div>
     </div>
-  </div>
-  @include('bottomNav')
+    @include('bottomNav')
 @endsection
