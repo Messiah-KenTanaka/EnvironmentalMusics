c8f510e18b4f766aa5dd2dc0b05422b533b19d16
KenTanaka <k.tanaka@apptime.co.jp>
2023-11-28 11:28:48 +0900

    全国釣り場情報取得のAPIを変更

--- app/Http/Controllers/PlaceMapController.php
+++ app/Http/Controllers/PlaceMapController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\PlaceMap;
 
 class PlaceMapController extends Controller
 {
     public function index() {
-
-        return PlaceMap::all();
+        // 全国のフィールドMAP情報を取得
+        try {
+            $placeMap = PlaceMap::all();
+            return response()->json([
+                'status' => 1,
+                'data' => $placeMap
+            ]);
+        } catch (\Exception $e) {
+            \Log::error($e->getMessage());
+            return response()->json([
+                'error_flag' => 1,
+                'message' => '全国釣り場情報の取得に失敗しました。'
+            ]);
+        }
     }
 }
